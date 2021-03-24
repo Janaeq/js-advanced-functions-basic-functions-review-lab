6e09e6a7febcc73087efbadd510b9b639b6e3965
Janae Quinones <janaemassa@gmail.com>
2021-03-24 00:41:29 -0400

    commit tree c91373c67150b62300bab699ca726c52d9d3d090

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+
+// Function Declaration
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+// Function Expression
+const mondayWork = function(task = 'go to the office') {
+    return `This Monday, I will ${task}.`
+}
+
+// Closures
+function wrapAdjective(visualFlair = "*") {
+    return function(adjective = "special") {
+        return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+}
+
+const Calculator = {
+    add: function (a, b) {
+        return a + b
+    },
+    subtract: function (a, b) {
+        return a - b
+    },
+    multiply: function (a, b) {
+        return a * b
+    },
+    divide: function (a, b) {
+        return a / b
+    }
+}
+
+function actionApplyer(int, arr) {
+    if (arr === []) {
+        return int
+    } else {
+        return arr.reduce(function (i, f) {
+            return f(i)
+        }, int)
+    }
+}
